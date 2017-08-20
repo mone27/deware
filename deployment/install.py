40c04b34b02dab0425fb6cd42054a49fece02aa5
mone27 <mone.massaro@gmail.com>
2017-08-20 12:34:57 +0200

    added based template support for installation(.desktop and .service)

--- deployment/install.py
+++ deployment/install.py
@@ -5,4 +5,14 @@ Created on Wed Aug  9 17:13:26 2017
 
 @author: simone
 """
-
+#should do somwthing else before
+#loads template for service and desktop file
+OPTIONS = {
+    install_location = 'prova',
+    python = 'usr/bin/python3'
+}
+from string import Template
+with open('deware.desktop.tmpl') as t:
+    out_f = open('deware.desktop',mode='w')
+    out_f.write(Template(t.read()).substitute(OPTIONS))
+    out_f.close()
