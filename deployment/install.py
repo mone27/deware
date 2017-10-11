8ee6b4fdd70b89f88f08e6a710762ba9efa1b567
mone27 <mone.massaro@gmail.com>
2017-10-11 12:44:33 +0200

    small refactor on install.py

--- deployment/install.py
+++ deployment/install.py
@@ -5,14 +5,25 @@ Created on Wed Aug  9 17:13:26 2017
 
 @author: simone
 """
+from string import Template
+
 #should do somwthing else before
 #loads template for service and desktop file
 OPTIONS = {
-    install_location = 'prova',
-    python = 'usr/bin/python3'
+    'install_location': 'prova',
+    'python': 'usr/bin/python3'
 }
-from string import Template
-with open('deware.desktop.tmpl') as t:
-    out_f = open('deware.desktop',mode='w')
-    out_f.write(Template(t.read()).substitute(OPTIONS))
-    out_f.close()
+templates = [
+    'deware.desktop.tmpl',
+    'dewared.service.tmpl'
+]
+def file_template(template_name):
+    """ loads a string template from a file (must have .tmpl extension)
+    generating corresponding file without .tmpl extension.
+    """
+    with open(template_name) as t, open(template_name[:-5],mode='w') as out_f:
+        out_f.write(Template(t.read()).substitute(OPTIONS))
+
+
+if __name__ == "__main__":
+    [file_template(__) for __ in templates]
