786e4bcc13cac3c932c44e952ed330d2c4d4e125
mone27 <mone.massaro@gmail.com>
2017-12-04 19:05:52 +0100

    basic conda setup on raspberry

---
+++ deployment/berryconda_install.sh
@@ -0,0 +1,4 @@
+ wget https://github.com/jjhelmus/berryconda/releases/download/v2.0.0/Berryconda3-2.0.0-Linux-armv7l.sh
+ bash Berryconda3-2.0.0-Linux-armv7l.sh -b
+ source berryconda3/bin/activate
+ 
