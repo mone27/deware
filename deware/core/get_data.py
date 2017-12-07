b23244787c6f3db4f15fbdf6a320c2fa909e471e
mone27 <mone.massaro@gmail.com>
2017-12-07 19:20:05 +0100

    started moveing to en variable for settings

--- deware/core/get_data.py
+++ deware/core/get_data.py
@@ -5,21 +5,43 @@
 """driver that reads from serial port
     and sends data on zmq pub socket"""
 from multiprocessing import Process
-from settings import SerialInput as setg
+import deware.core.settings as setg
+dir(setg)
 import time
 import serial
 import zmq
-class SerialInput(Process):
+import json
+from logging import info, debug, warning
+
+class sensors_read(Process):
     def __init__(self):
         Process.__init__(self)
-    def run(self):
-        # open serial port # just another stupid comment!!!!
-        SerPort = serial.Serial(setg.SerialPort)
+        try:
+            self.ser_port = serial.Serial(setg.serial_port)
+        except serial.SerialException as msg:
+            warning(msg)
         # zmq inizialization
-        ctx = zmq.Context.instance()
-        PubSock = ctx.socket(zmq.PUB)
-        PubSock.bind(f"tcp://127.0.0.1:{setg.PubPort}")
+        self.ctx = zmq.Context.instance()
+        self.pub_socket = self.ctx.socket(zmq.PUB)
+        self.pub_socket.bind(f"tcp://127.0.0.1:{setg.pub_port}")
+    def run(self):
         while True:
+            try:
+                data = self.ser_port.readline().deconde()
+
+            except serial.SerialException as msg:
+                warning(msg)
+
+        # open serial port # just another stupid comment!!!!
+
+
+
+
+
+
+#        while True:
+            #try:
+
 # this is kept for hystorical reason
 #            data = ''
 #            for c in SerPort.readline():
@@ -33,8 +55,8 @@ class SerialInput(Process):
 #            else:
 #                # raise error to implement
 #                pass
-            time.sleep(2)
+             #time.sleep(2)
 
 if __name__ == '__main__':
-    proc = SerialInput()
+    proc = sensors_read()
     proc.start()
--- deware/core/settings.py
+++ deware/core/settings.py
@@ -1,7 +1,11 @@
 # -*- coding: utf-8 -*-
+from  os import environ
+pub_port = 7001
+serial_port = environ.get("DEWARE_SERIAL_DEVICE", "/dev/arduino_uno")
 class SerialInput():
-    SerialPort = '/dev/arduino_uno'
-    PubPort = 7001
+#   serial device must be set as a udev rule ... to fix in future
+
+
     Random = True
 class Db():
     dbFile = '../prova.sqlite'
