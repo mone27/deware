57db1ae40249a1ee2bde040d1d87f7a1c5d282b0
mone27 <mone.massaro@gmail.com>
2018-01-01 17:49:02 +0100

    updated settings to avoid crash if no device present

--- deware/core/settings.py
+++ deware/core/settings.py
@@ -2,7 +2,13 @@
 from  os import environ
 from . utilis import serial_ports
 pub_port = 7002
-serial_port = environ.get("DEWARE_SERIAL_DEVICE", serial_ports()[0])
+scanned_serial_port = '/dev/zero'
+try:
+    scanned_serial_port = serial_ports()[0]
+except IndexError:
+    #means no port found
+    pass
+serial_port = environ.get("DEWARE_SERIAL_DEVICE", scanned_serial_port)
 
 db_file = "prova.sqlite"
 time = 2
